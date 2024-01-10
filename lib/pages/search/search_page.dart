37751dbf1844ab2972e2ff592f9a9d217e03647e
bimantaratirta <bimantarats@gmail.com>
2024-01-10 17:52:04 +0700

    Implement API at Search Page

--- lib/pages/search/search_page.dart
+++ lib/pages/search/search_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
+import 'package:get/route_manager.dart';
 
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
+import '../../routes/app_routes.dart';
 import '../../shared/widget/app_textfield.dart';
 import '../../shared/widget/book_card_horz.dart';
+import '../../shared/widget/book_card_skeleton.dart';
+import '../../shared/widget/empty_list.dart';
 import '../../theme/app_color.dart';
 import '../../theme/app_text_stlye.dart';
+import '../../utils/compute_luminance.dart';
 import 'controller/search_page_controller.dart';
 
 class SearchPage extends StatelessWidget {
@@ -15,17 +20,18 @@ class SearchPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final controller = Get.find<SearchPageController>();
-    const listBuku = [1, 1, 1, 1, 1, 1, 1, 1, 1];
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("Pencarian"),
         actions: [
           IconButton(
-            onPressed: controller.showFilterModal,
-            icon: SvgPicture.asset(
-              "assets/icons/filter.svg",
+            onPressed: controller.sort,
+            icon: Icon(
+              Icons.swap_vert_rounded,
+              color: calculateLuminance(theme.primaryColor),
             ),
           ),
           HGap.s,
@@ -41,6 +47,7 @@ class SearchPage extends StatelessWidget {
               controller: controller.textController,
               focusNode: controller.searchFocusNode,
               onTapOutside: (_) => controller.searchFocusNode.unfocus(),
+              onChanged: controller.search,
               isError: false,
               contentPadding: const EdgeInsets.symmetric(vertical: Sizes.s, horizontal: Sizes.r),
               label: Text(
@@ -51,20 +58,92 @@ class SearchPage extends StatelessWidget {
           ),
           VGap.s,
           Expanded(
-            child: ListView.builder(
-              padding: const EdgeInsets.symmetric(horizontal: Sizes.m),
-              shrinkWrap: true,
-              itemCount: 10,
-              itemBuilder: (ctx, idx) {
-                if (idx == 0 || idx == listBuku.length) return VGap.r;
+            child: Obx(() {
+              final books = controller.books.value;
+              if (books == null) {
+                return ListView.builder(
+                  padding: const EdgeInsets.all(Sizes.m),
+                  shrinkWrap: true,
+                  itemCount: 10,
+                  itemBuilder: (ctx, idx) {
+                    return Column(
+                      children: [
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const BookCardSkeleton(),
+                            HGap.m,
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: 25,
+                                    decoration: const BoxDecoration(
+                                      color: AppColor.lightGrey,
+                                      borderRadius: BorderRadius.all(Radius.circular(Sizes.s)),
+                                    ),
+                                  ),
+                                  VGap.s,
+                                  Container(
+                                    height: 25,
+                                    decoration: const BoxDecoration(
+                                      color: AppColor.lightGrey,
+                                      borderRadius: BorderRadius.all(Radius.circular(Sizes.s)),
+                                    ),
+                                  ),
+                                  VGap.s,
+                                  Container(
+                                    height: 25,
+                                    decoration: const BoxDecoration(
+                                      color: AppColor.lightGrey,
+                                      borderRadius: BorderRadius.all(Radius.circular(Sizes.s)),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                        VGap.s,
+                      ],
+                    );
+                  },
+                );
+              }
+              if (books.isEmpty) {
                 return const Column(
                   children: [
-                    BookCardHorz(),
-                    VGap.s,
+                    VGap.m,
+                    EmptyList(
+                      description: "Buku yang Anda cari tidak ada",
+                    ),
                   ],
                 );
-              },
-            ),
+              }
+              return ListView.builder(
+                padding: const EdgeInsets.all(Sizes.m),
+                shrinkWrap: true,
+                itemCount: books.length,
+                itemBuilder: (ctx, idx) {
+                  final book = books[idx].buku;
+                  return Column(
+                    children: [
+                      BookCardHorz(
+                        id: book?.id ?? "-",
+                        judul: book?.judul ?? "-",
+                        penulis: book?.penulis ?? "-",
+                        idSampul: book?.assetSampulId ?? "-",
+                        copy: "${books[idx].jumlahSiapPinjam ?? '-'}",
+                        harga: (int.parse(book?.hargaSewa ?? "0") / 100).toString(),
+                        onTap: () => Get.toNamed(AppRoutes.book, arguments: books[idx]),
+                      ),
+                      VGap.s,
+                    ],
+                  );
+                },
+              );
+            }),
           ),
         ],
       ),

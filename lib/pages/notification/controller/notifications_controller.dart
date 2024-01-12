67e0369371b6c717539cf9528bc8ce2d0416d361
bimantaratirta <bimantarats@gmail.com>
2024-01-12 11:11:05 +0700

    Remove unused comments, add some gaps to read confirm button

--- lib/pages/notification/controller/notifications_controller.dart
+++ lib/pages/notification/controller/notifications_controller.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:get/route_manager.dart';
 
+import '../../../constants/sizes.dart';
 import '../../../shared/widget/app_button.dart';
 
 class NotificationsController extends GetxController {
@@ -9,7 +10,7 @@ class NotificationsController extends GetxController {
     Get.dialog(
       AlertDialog(
         title: const Text("Apa Anda yakin?"),
-        content: const Text("Seluruh notifikasi akan ditandai telah dibaca setelah"),
+        content: const Text("Seluruh notifikasi akan ditandai telah dibaca setelah ini"),
         actions: [
           AppButton(
             type: ButtonType.text,
@@ -18,6 +19,7 @@ class NotificationsController extends GetxController {
           ),
           AppButton(
             type: ButtonType.elevated,
+            padding: const EdgeInsets.symmetric(vertical: Sizes.sr, horizontal: Sizes.m),
             onPressed: () {},
             child: const Text("Ya"),
           ),

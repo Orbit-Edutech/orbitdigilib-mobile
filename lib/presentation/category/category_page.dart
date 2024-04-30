6c634058e1413a52c000e74947727353886621a1
bimantaratirta <bimantarats@gmail.com>
2024-04-30 15:00:35 +0700

    Fix infinite scroll at category/catalog page

--- lib/presentation/category/category_page.dart
+++ lib/presentation/category/category_page.dart
@@ -76,11 +76,14 @@ class CategoryPage extends StatelessWidget {
               child: RefreshIndicator(
                 onRefresh: () async => await controller.onInit(),
                 child: SingleChildScrollView(
+                  controller: controller.scrollController,
                   padding: const EdgeInsets.only(bottom: Sizes.m),
                   physics: const AlwaysScrollableScrollPhysics(),
                   child: Obx(() {
                     final filteredBooks = controller.filteredBooks.value;
                     final _ = controller.asc.value; // Untuk trigger re-render
+                    // ignore: unused_local_variable
+                    final page = controller.page.value; // Untuk trigger re-render
                     if (filteredBooks == null) {
                       return AlignedGridView.count(
                         shrinkWrap: true,

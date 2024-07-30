cd3aab39816645dc332bef1b11261eeae189d791
bimantaratirta <bimantarats@gmail.com>
2024-07-31 03:33:47 +0700

    build: upgrade app version and build version

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -2,6 +2,6 @@ import '../api/auth/model/model_auth_validate.dart';
 
 class AppInfo {
   static const String name = "ORBIT DIGILIB";
-  static final Version android = Version(versionCode: 9, versionName: "0.2.4");
-  static final Version iOs = Version(versionCode: 1, versionName: "1.0.0");
+  static final Version android = Version(versionCode: 10, versionName: "0.3.0");
+  static final Version iOs = Version(versionCode: 2, versionName: "1.0.0");
 }

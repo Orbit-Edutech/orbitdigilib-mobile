92b8bf60d113943329ef7735513132c8b7c570d3
bimantaratirta <bimantarats@gmail.com>
2024-08-26 22:32:14 +0700

    fix: remove unused todo

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ void main() async {
   ImageUtils.prechacheImages();
 }
 
-// TODO: Screenshot masih delay pada iOS halaman membaca; Android manage external permission kena denied playstore
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 

fbd6914ef6f58d72a66e0bb7e79a0210a56c9b90
bimantaratirta <bimantarats@gmail.com>
2024-09-30 14:38:28 +0700

    hotfix: change base url to prod

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -2,6 +2,6 @@ import '../api/auth/model/model_auth_validate.dart';
 
 class AppInfo {
   static const String name = "ORBIT DIGILIB";
-  static final Version android = Version(versionCode: 15, versionName: "0.5.0");
-  static final Version iOs = Version(versionCode: 9, versionName: "1.2.0");
+  static final Version android = Version(versionCode: 16, versionName: "0.5.1");
+  static final Version iOs = Version(versionCode: 10, versionName: "1.2.1");
 }

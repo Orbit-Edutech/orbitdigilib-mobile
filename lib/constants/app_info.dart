d591f784d145bf1a993b9040ae7f219cf6ffd6c5
bimantaratirta <bimantarats@gmail.com>
2024-09-30 13:55:47 +0700

    build: upgrade both android and ios minor version

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -2,6 +2,6 @@ import '../api/auth/model/model_auth_validate.dart';
 
 class AppInfo {
   static const String name = "ORBIT DIGILIB";
-  static final Version android = Version(versionCode: 14, versionName: "0.4.1");
-  static final Version iOs = Version(versionCode: 8, versionName: "1.1.2");
+  static final Version android = Version(versionCode: 15, versionName: "0.5.0");
+  static final Version iOs = Version(versionCode: 9, versionName: "1.2.0");
 }

a3e1241ad889e6abe4fb895fa718054a9be16cb4
bimantaratirta <bimantarats@gmail.com>
2025-11-20 02:31:53 +0700

    fix: removing unused 'hide'

--- lib/presentation/index/widgets/index_banner.dart
+++ lib/presentation/index/widgets/index_banner.dart
@@ -1,6 +1,6 @@
 import "package:cached_network_image/cached_network_image.dart";
 import "package:carousel_slider/carousel_slider.dart";
-import "package:flutter/material.dart" hide CarouselController;
+import "package:flutter/material.dart";
 import "package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart";
 import "package:get/instance_manager.dart";
 import "package:get/route_manager.dart";

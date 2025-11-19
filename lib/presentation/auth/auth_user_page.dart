4a05caf424ddf05fd55c05833f010b1f6389e577
bimantaratirta <bimantarats@gmail.com>
2025-11-20 02:31:38 +0700

    feat: auth feature responsive

--- lib/presentation/auth/auth_user_page.dart
+++ lib/presentation/auth/auth_user_page.dart
@@ -9,6 +9,7 @@ import '../../api/perpustakaan/model/model_perpustakaan.dart';
 import '../../constants/gaps.dart';
 import '../../constants/sizes.dart';
 import '../../theme/app_text_stlye.dart';
+import '../../utils/responsive_helper.dart';
 import 'controller/auth_user_controller.dart';
 import 'widgets/auth_forgot_form.dart';
 import 'widgets/auth_login_form.dart';
@@ -26,56 +27,75 @@ class AuthUserPage extends StatelessWidget {
     return PopScope(
       canPop: false,
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: theme.primaryColor,
-                  borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(Sizes.m),
-                    bottomRight: Radius.circular(Sizes.m),
-                  ),
+        body: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              maxWidth: ResponsiveHelper.getMaxContentWidth(context),
+            ),
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: size.height,
                 ),
-                width: size.width,
-                padding: const EdgeInsets.all(24),
-                child: SafeArea(
+                child: IntrinsicHeight(
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: const BorderRadius.all(Radius.circular(100)),
-                          image: perpustakaan.assetLogo != null
-                              ? DecorationImage(
-                                  image: CachedNetworkImageProvider(APIPath.publicAsset(perpustakaan.assetLogo?.id ?? "")),
-                                  fit: BoxFit.cover,
-                                )
-                              : null,
-                        ),
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: theme.primaryColor,
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(Sizes.m),
+                        bottomRight: Radius.circular(Sizes.m),
                       ),
-                      VGap.m,
-                      Text(
-                        controller.perpustakaan?.nama ?? "-",
-                        style: AppTextStyle.ts16Bold.copyWith(color: Colors.white),
+                    ),
+                    width: size.width,
+                    padding: EdgeInsets.all(
+                      ResponsiveHelper.getHorizontalPadding(context),
+                    ),
+                    child: SafeArea(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: const BorderRadius.all(Radius.circular(100)),
+                              image: perpustakaan.assetLogo != null
+                                  ? DecorationImage(
+                                      image:
+                                          CachedNetworkImageProvider(APIPath.publicAsset(perpustakaan.assetLogo?.id ?? "")),
+                                      fit: BoxFit.cover,
+                                    )
+                                  : null,
+                            ),
+                          ),
+                          VGap.m,
+                          Text(
+                            controller.perpustakaan?.nama ?? "-",
+                            style: AppTextStyle.ts16Bold.copyWith(color: Colors.white),
+                          ),
+                          VGap.m,
+                        ],
                       ),
-                      VGap.m,
-                    ],
+                    ),
+                  ),
+                  VGap.l,
+                  Obx(() {
+                    final isforgot = controller.isForgot.value;
+                    return Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: ResponsiveHelper.getHorizontalPadding(context),
+                      ),
+                      child: isforgot ? AuthForgotForm(controller: controller) : AuthLoginForm(controller: controller),
+                    );
+                  })
+                ],
                   ),
                 ),
               ),
-              VGap.l,
-              Obx(() {
-                final isforgot = controller.isForgot.value;
-                return Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: Sizes.m),
-                  child: isforgot ? AuthForgotForm(controller: controller) : AuthLoginForm(controller: controller),
-                );
-              })
-            ],
+            ),
           ),
         ),
       ),
